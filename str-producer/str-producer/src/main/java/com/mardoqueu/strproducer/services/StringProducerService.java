9c20a333dcceb9621b0e95d6fca8a485796c557d
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-22 11:50:29 -0300

    add callback function to sendMessage function

--- str-producer/str-producer/src/main/java/com/mardoqueu/strproducer/services/StringProducerService.java
+++ str-producer/str-producer/src/main/java/com/mardoqueu/strproducer/services/StringProducerService.java
@@ -1,10 +1,11 @@
 package com.mardoqueu.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -12,6 +13,15 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("stc-topic", message);
+        kafkaTemplate.send("stc-topic", message).addCallback(
+                success -> {
+                    if(success != null){
+                        log.info("Send message with success {}", message);
+                        log.info("Partition {} Offset{}", success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send message")
+        );
     }
 }
